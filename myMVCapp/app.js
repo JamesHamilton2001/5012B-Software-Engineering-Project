fbef2f48b8a9faae297d4b6eb6127de85d6916b5
aillsley <adamillsley2012@gmail.com>
2023-05-07 18:21:10 +0100

    added dashboard view

--- myMVCapp/app.js
+++ myMVCapp/app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var login = require('./routes/login');
+var dashboard = require('./routes/dashboard')
 
 var app = express();
 
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/login', login);
+app.use('/dashboard', dashboard)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
