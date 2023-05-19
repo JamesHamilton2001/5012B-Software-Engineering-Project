d7c146f04b8d61ef6f915816dd68a595c019abed
Joey Sabey <joey.sabey@gmx.com>
2023-05-19 09:13:07 +0100

    add very basic new model/User.js - literally just can pull a user by username from the db

---
+++ model/User.js
@@ -0,0 +1,31 @@
+import sqlite3 from 'sqlite3';
+import { open } from 'sqlite';
+
+global.db = await open({
+   filename: 'debug.db',
+   driver: sqlite3.cached.Database
+});
+
+export default class User {
+   constructor(data) {
+      // JCopy/clone the properties from the given object to the new User object.
+      // This should be considered garbage in; garbage out, so the factory method(s)
+      // should be used instead of the constructor directly.
+      Object.assign(this, data);
+
+      // TODO: load things like groups, latest weight, etc. here? Probably best to load
+      // those as requested instead perhaps..?
+   }
+
+   // Main factory method. Pulls the basic user data associated with a given username
+   // from the database, throwing an error if the user is not found. (Note, idk really
+   // if and _when_ that error will fire, or exactly what will happen to node without
+   // some testing.)
+   static async getByUsername(username) {
+      var row = await db.get('SELECT * FROM user WHERE username = ?', username);
+      if(row === undefined)
+         throw new Error('User not found:' + username);
+      return new User(row);
+   }
+}
+
