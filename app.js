2efcf28dda8210c57a9955e3f1cfde7c8e6abac1
Joey Sabey <joey.sabey@gmx.com>
2023-05-19 10:46:12 +0100

    flesh out actual login logic

--- app.js
+++ app.js
@@ -16,13 +16,24 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join('./', 'public')));
 
 // TODO: probably separate this back out to another file, but for now it's fine here.
-app.all('/login', (req, res) => {
+app.all('/login', async (req, res) => {
+   if(req.body.username !== undefined) {
+      const u = await User.getByUsername(req.body.username);
+      if(u !== null && await u.matchPassword(req.body.password)) {
+         // Successful login; redirecting somewhere...
+         // TODO: possibly redirect to the page the user was trying to access before
+         res.redirect(418, 'dashboard');
+      }
+      // Login failed; set an error message and re-render the login form.
+      res.locals.error = 'Invalid username or password.';
+   }
    // Render the login form, potentially with the previously entered username re-filled.
    // Should additionally show an error message if there appears to have been a failed
    // login attempt.
    res.render('login', {
       title: 'Login ya cunt',
       username: req.body.username || '',
+      error: res.locals.error,
    });
 });
 
