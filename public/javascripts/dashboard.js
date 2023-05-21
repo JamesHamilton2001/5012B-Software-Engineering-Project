a3d1139727c4e98c2e60b428bb36c5a04a50ce37
JamesHamilton2001 <james.hamilton.2001@outlook.com>
2023-05-21 23:27:09 +0100

    reformat dashboard body resize function
    call me mr janitor

--- public/javascripts/dashboard.js
+++ public/javascripts/dashboard.js
@@ -1,13 +1,12 @@
+function fixDashboardBody() {
+  var headerHeight = document.querySelector("header").offsetHeight;
+  var windowHeight = window.innerHeight;
+  document.body.style.height = (windowHeight - headerHeight) + "px";
+}
 
-//  on load and resize dashboard body to fit size of screen - header
+// on load and resize dashboard body to fit size of screen - header
 document.addEventListener("DOMContentLoaded", function()
 {
-  function fixDashboardBody() {
-    var headerHeight = document.querySelector("header").offsetHeight;
-    var windowHeight = window.innerHeight;
-    document.body.style.height = (windowHeight - headerHeight) + "px";
-  }
   fixDashboardBody();
-
   window.addEventListener("resize", fixDashboardBody);
 });
