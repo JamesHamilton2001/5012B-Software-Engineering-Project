8a492f9fcac82de6987e2f87c0050fb059d614f8
Joey Sabey <joey.sabey@gmx.com>
2023-05-23 07:22:50 +0100

    rough draft/skeleton Exercise

---
+++ model/Exercise.js
@@ -0,0 +1,31 @@
+import sqlite3 from 'sqlite3';
+import { open } from 'sqlite';
+
+export default class Exercise {
+   constructor(data) {
+      // JCopy/clone the properties from the given object to the new Exercise object.
+      // This should be considered garbage in; garbage out, so the factory method(s)
+      // should be used instead of the constructor directly.
+      Object.assign(this, data);
+
+      // TODO: load/init other shit?
+   }
+
+
+   // Main factory method. Pulls all exercise sessions associated with a given user
+   // from the database, and returns them as an array of Exercise objects.
+   static async getByUserID(user_id) {
+      const rows = await db.all('SELECT * FROM user_exercise_session WHERE user_id = ?', user_id);
+      if(rows === undefined)
+         return null;
+      return rows.map(x => new Exercise(x));
+   }
+
+
+   // Return list of all exercise types in the database.
+   static async getTypes() {
+      const sql = 'SELECT * FROM exercise_type';
+      return db.all(sql);
+   }
+}
+
