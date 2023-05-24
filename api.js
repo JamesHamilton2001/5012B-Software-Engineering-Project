639c9b9ed81ef0085d0d7fba1be7ff3718f70766
Joey Sabey <joey.sabey@gmx.com>
2023-05-24 13:14:53 +0100

    move /user/weight get/post into a single router.route()

--- api.js
+++ api.js
@@ -19,18 +19,18 @@ router.get('/user', (req, res) => {
 });
 
 
-// Return weight data for the currently logged in user.
-router.get('/user/weight', async (req, res) => {
-   const data = await res.locals.user.getWeight(req.query.limit, req.query.start, req.query.end);
-   res.json(data);
-});
-
-
-// Add a new weight record for the currently logged in user.
-router.post('/user/weight', async (req, res) => {
-   const data = await res.locals.user.addWeight(req.body.weight, Math.floor(Date.now() / 1000));
-   res.status(201).json("weight record added");
-});
+// Access the current user's weight data.
+router.route('/user/weight')
+   // Return weight data for the currently logged in user.
+   .get(async (req, res) => {
+      const data = await res.locals.user.getWeight(req.query.limit, req.query.start, req.query.end);
+      res.json(data);
+   });
+   // Add a new weight record for the currently logged in user.
+   .post(async (req, res) => {
+      const data = await res.locals.user.addWeight(req.body.weight, Math.floor(Date.now() / 1000));
+      res.status(201).json("weight record added");
+   });
 
 
 export default router;
