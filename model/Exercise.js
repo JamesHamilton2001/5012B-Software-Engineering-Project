877afd52eb1bf33ffbb104b131f9024b2d0c5ad2
Joey Sabey <joey.sabey@gmx.com>
2023-05-24 15:18:35 +0100

    typo

--- model/Exercise.js
+++ model/Exercise.js
@@ -3,7 +3,7 @@ import { open } from 'sqlite';
 
 export default class Exercise {
    constructor(data) {
-      // JCopy/clone the properties from the given object to the new Exercise object.
+      // Copy/clone the properties from the given object to the new Exercise object.
       // This should be considered garbage in; garbage out, so the factory method(s)
       // should be used instead of the constructor directly.
       Object.assign(this, data);
