33d6123b45a162b68c37ae7f1beb47bc2fd6752c
Joey Sabey <joey.sabey@gmx.com>
2023-05-24 21:35:32 +0100

    add draft meal_item_view

--- sql/views.sql
+++ sql/views.sql
@@ -11,3 +11,21 @@ CREATE VIEW exercise_view AS
    INNER JOIN exercise_type
       ON exercise_type_id = exercise_type.id
 ;
+
+CREATE VIEW meal_item_view AS
+   SELECT
+      user_meal_item.id,
+      user_meal_id
+         AS meal_id,
+      food_type_id
+         AS type_id,
+      food_type.name
+         AS type,
+      quantity,
+      calories_per_100g,
+      calories_per_100g * quantity / 100
+         AS calories
+   FROM user_meal_item
+   INNER JOIN food_type
+      ON food_type_id = food_type.id
+;
