3575f30e5fc7bedf050fb748c1ec046afce7c2b1
aillsley <adamillsley2012@gmail.com>
2023-05-24 18:37:11 +0100

    Goal.js created
    Class created and add method made to add goals to the goal table

---
+++ model/Goal.js
@@ -0,0 +1,35 @@
+import * as bcrypt from 'bcrypt';
+import sqlite3 from 'sqlite3';
+import { open } from 'sqlite';
+
+export default class Goal {
+  constructor(data){
+    Object.assign(this, data);
+
+      // TODO: load/init other shit?
+   }
+   //add method that creates a new goal, taking
+   //user_id, target, exercise_type_id, end_time
+   //from goal form
+   //!needs testing!
+   static async add(user_id, target, exercise_type_id, end_time){
+    const sql = 'INSERT INTO goal(user_id, exercise_type_id, target, start_time, end_time) VALUES(:user_id, :exercise_type_id, :target, :start_time, :end_time)';
+    const args = {
+      ':user_id': user_id,
+      ':target': target,
+      ':exercise_type_id': exercise_type_id,
+      ':start_time': Math.floor(Date.now() / 1000),
+      ':end_time': end_time,
+   };
+   const data = db.run(sql, args);
+   return data;
+  }
+   // Main factory method.
+  
+   static async getByUserID(user_id) {
+      const rows = await db.all('SELECT * FROM goal WHERE user_id = ?', user_id);
+      if(rows === undefined)
+         return null;
+      return rows.map(x => new Goal(x));
+   }
+}
