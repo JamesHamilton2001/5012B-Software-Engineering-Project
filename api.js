59fe06eac43601a4ea90a7e22be58073d1996554
Joey Sabey <joey.sabey@gmx.com>
2023-05-24 13:28:24 +0100

    fix chained route

--- api.js
+++ api.js
@@ -26,12 +26,12 @@ router.route('/user/weight')
    .get(async (req, res) => {
       const data = await res.locals.user.getWeight(req.query.limit, req.query.start, req.query.end);
       res.json(data);
-   });
+   })
    // Add a new weight record for the currently logged in user.
    .post(async (req, res) => {
       const data = await res.locals.user.addWeight(req.body.weight, Math.floor(Date.now() / 1000));
       res.status(201).json("weight record added");
-   });
+   })
 
 
 export default router;
