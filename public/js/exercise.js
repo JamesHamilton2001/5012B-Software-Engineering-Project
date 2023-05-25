2c7962a26931db3c79b9f720abae2623b064c648
Joey Sabey <joey.sabey@gmx.com>
2023-05-25 22:10:36 +0100

    drafted some front end utility code for manipulating exercise data

---
+++ public/js/exercise.js
@@ -0,0 +1,46 @@
+// Frontend utility code for exercise data
+
+import * as api from './api.js';
+
+export const types = await api.get('exercise/types');
+
+export function createTypeSelect(cb) {
+   const sel = document.createElement('select');
+   const placeholder = sel.appendChild(new Option('Exercise type…', 0, true, true));
+   sel.append(...types.map(t => new Option(t.name, t.id, false, false)));
+
+   // Hide the placeholder option from the actual dropdown box/list
+   sel.addEventListener('click', () => placeholder.style.display = 'none');
+
+   // Dispose of the placeholder option once a selection is made
+   sel.addEventListener('change', e => e.target.remove(0), {once: true});
+   sel.addEventListener('change', e => e.exercise = types[sel.value - 1]);
+   sel.addEventListener('change', cb);
+   return sel;
+};
+
+function createForm() {
+   const form = document.createElement('form');
+   const metricLabel = document.createElement('span');
+   const typeSelect = form.appendChild(createTypeSelect(e => {
+      metricLabel.textContent = e.exercise.metric;
+   }));
+   const valueBox = form.appendChild(document.createElement('div'));
+   const valueInput = valueBox.appendChild(document.createElement('input'));
+   valueBox.append(metricLabel);
+   form.id = 'exerciseForm';
+   typeSelect.id = 'typeSelect';
+   valueBox.id = 'valueBox';
+   valueInput.id = 'valueInput';
+   valueInput.value = '17.52';
+   metricLabel.id = 'metricLabel';
+   form.style.border = '1px solid black';
+   return form;
+}
+
+const cont = document.getElementById('container');
+cont.appendChild(createForm());
+//cont.appendChild(createTypeSelect(e => {
+//   console.log(e.exercise);
+//}));
+
