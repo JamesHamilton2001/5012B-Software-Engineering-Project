67c2fbbd947959b8570ced8711a1b3d7de0ec9aa
aillsley <adamillsley2012@gmail.com>
2023-05-27 16:27:41 +0100

    had an attempt at getting the data to save

--- app.js
+++ app.js
@@ -59,10 +59,6 @@ app.get('/', (req, res) => {
   res.render('index', { title: 'HealthMate App' });
 });
 
-app.get('/login' ,  (req, res) => {
-  res.render('login', { title: 'Login' });
-});
-
 app.get('/dashboard' , (req, res) => {
   res.render('dashboard', { title: 'My Dashboard' });
 });
@@ -79,6 +75,16 @@ app.get('/exercise' , (req, res) => {
 app.get('/goal', (req, res) => {
   res.render('goal' ,{title: 'New Goal'});
 })
+
+// app.post('/newGoal', (req,res) => {
+//   res.send('Goal data recieved')
+   
+//   let exerciseType = req.body.exerciseType
+//   let target = req.
+//   console.log(target)
+//   //Goal.add();
+//   // console.log(res)
+// })
 
 
 export { app as app };
