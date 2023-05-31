c3c34fc0588179176f78ac06a98b5555c915977e
Joey Sabey <joey.sabey@gmx.com>
2023-05-31 10:19:30 +0100

    rework the selection logic in dashboard.js to use the fancy new (over 10 years old, apparently) data-* html attributes feature

--- public/javascripts/dashboard.js
+++ public/javascripts/dashboard.js
@@ -9,21 +9,11 @@ const displayElement = document.querySelector('body > main');
 
 // add event listener to each sidebar item, that changes the dashboard display accordingly
 document.querySelectorAll('menu > li').forEach((item) => {
- var selection;
- switch(item.id) {
-   case "slct_stats":    { selection = "stats"; break; }
-   case "slct_groups":   { selection = "groups"; break; }
-   case "slct_goals":    { selection = "goals"; break; }
-   case "slct_exercise": { selection = "exercise"; break; }
-   case "slct_meals":    { selection = "meals"; break; }
-   case "slct_weight":   { selection = "weight"; break; }
- }
-
- item.addEventListener('click', () => executeSelectionScript(selection), { once: true });
+ item.addEventListener('click', () => executeSelectionScript(item.dataset.display), { once: true });
 
  item.addEventListener('click', () => {
     // fetch dashboard display html corresponding to sidebar selection
-    fetch("/dashboard/" + selection)
+    fetch("/dashboard/" + item.dataset.display)
       // convert html prototype into text
       .then((response) => { return response.text(); })
       // insert html text into display div inner html and load script
