e24cc5438657182638036054249d823b2b1f8b14
Joey Sabey <joey.sabey@gmx.com>
2023-05-31 13:31:58 +0100

    remove executeSelectionScript() - cleaner to just embed any & all scripts in the partials

--- public/javascripts/dashboard.js
+++ public/javascripts/dashboard.js
@@ -1,16 +1,7 @@
-// execute..? this doesn't execute anything; it adds a new script file to the DOM
-function executeSelectionScript(name) {
-  var script = document.createElement("script");
-  script.src = "javascripts/dashboard/" + name + ".js";
-  document.head.appendChild(script);
-}
-
 const displayElement = document.querySelector('body > main');
 
 // add event listener to each sidebar item, that changes the dashboard display accordingly
 document.querySelectorAll('menu > li').forEach((item) => {
- item.addEventListener('click', () => executeSelectionScript(item.dataset.display), { once: true });
-
  item.addEventListener('click', () => {
     // fetch dashboard display html corresponding to sidebar selection
     fetch("/dashboard/" + item.dataset.display)
