0acc837aa733df12b0ee1de18f0f6ba28297407f
Joey Sabey <joey.sabey@gmx.com>
2023-05-31 05:29:57 +0100

    fix html/script loading behaviour

--- public/javascripts/dashboard.js
+++ public/javascripts/dashboard.js
@@ -1,3 +1,4 @@
+// execute..? this doesn't execute anything; it adds a new script file to the DOM
 function executeSelectionScript(name) {
   var script = document.createElement("script");
   script.src = "javascripts/dashboard/" + name + ".js";
@@ -9,33 +10,33 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // add event listener to each sidebar item, that changes the dashboard display accordingly
   document.querySelectorAll('menu > li').forEach((item) => {
-    item.addEventListener("mousedown", function() {
+    var selection;
+    switch(item.id)
+    {
+      case "slct_stats":    { selection = "stats"; break; }
+      case "slct_groups":   { selection = "groups"; break; }
+      case "slct_goals":    { selection = "goals"; break; }
+      case "slct_exercise": { selection = "exercise"; break; }
+      case "slct_meals":    { selection = "meals"; break; }
+      case "slct_weight":   { selection = "weight"; break; }
+    }
 
-      var selection;
-      switch(this.id)
-      {
-        case "slct_stats":    { selection = "stats"; break; }
-        case "slct_groups":   { selection = "groups"; break; }
-        case "slct_goals":    { selection = "goals"; break; }
-        case "slct_exercise": { selection = "exercise"; break; }
-        case "slct_meals":    { selection = "meals"; break; }
-        case "slct_weight":   { selection = "weight"; break; }
-      }
+    item.addEventListener('click', () => executeSelectionScript(selection), { once: true });
 
-      // fetch dashboard display html corresponding to sidebar selection
-      fetch("/dashboard/" + selection)
+    item.addEventListener('click', () => {
+       // fetch dashboard display html corresponding to sidebar selection
+       fetch("/dashboard/" + selection)
 
-        // convert html prototype into text
-        .then((response) => { return response.text(); })
+         // convert html prototype into text
+         .then((response) => { return response.text(); })
 
-        // insert html text into display div inner html and load script
-        .then((html) => {
-          displayElement.innerHTML = html;
-          executeSelectionScript(selection);
-        })
+         // insert html text into display div inner html and load script
+         .then((html) => {
+           displayElement.innerHTML = html;
+         })
 
-        // catch and log error (if that somehow happens...)
-        .catch((error) => { console.error("Unable to load template: ", error); });
+         // catch and log error (if that somehow happens...)
+         .catch((error) => { console.error("Unable to load template: ", error); });
     });
   });
 });
