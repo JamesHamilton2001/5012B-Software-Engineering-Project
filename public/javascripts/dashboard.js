acf111798c9f094f01960ceaf5a2ea2ac127204d
Joey Sabey <joey.sabey@gmx.com>
2023-05-31 05:31:56 +0100

    defer dashboard.js properly - it isn't 2008 anymore

--- public/javascripts/dashboard.js
+++ public/javascripts/dashboard.js
@@ -5,38 +5,36 @@ function executeSelectionScript(name) {
   document.head.appendChild(script);
 }
 
-document.addEventListener("DOMContentLoaded", () => {
-  const displayElement = document.querySelector('body > main');
+const displayElement = document.querySelector('body > main');
 
-  // add event listener to each sidebar item, that changes the dashboard display accordingly
-  document.querySelectorAll('menu > li').forEach((item) => {
-    var selection;
-    switch(item.id)
-    {
-      case "slct_stats":    { selection = "stats"; break; }
-      case "slct_groups":   { selection = "groups"; break; }
-      case "slct_goals":    { selection = "goals"; break; }
-      case "slct_exercise": { selection = "exercise"; break; }
-      case "slct_meals":    { selection = "meals"; break; }
-      case "slct_weight":   { selection = "weight"; break; }
-    }
+// add event listener to each sidebar item, that changes the dashboard display accordingly
+document.querySelectorAll('menu > li').forEach((item) => {
+ var selection;
+ switch(item.id)
+ {
+   case "slct_stats":    { selection = "stats"; break; }
+   case "slct_groups":   { selection = "groups"; break; }
+   case "slct_goals":    { selection = "goals"; break; }
+   case "slct_exercise": { selection = "exercise"; break; }
+   case "slct_meals":    { selection = "meals"; break; }
+   case "slct_weight":   { selection = "weight"; break; }
+ }
 
-    item.addEventListener('click', () => executeSelectionScript(selection), { once: true });
+ item.addEventListener('click', () => executeSelectionScript(selection), { once: true });
 
-    item.addEventListener('click', () => {
-       // fetch dashboard display html corresponding to sidebar selection
-       fetch("/dashboard/" + selection)
+ item.addEventListener('click', () => {
+    // fetch dashboard display html corresponding to sidebar selection
+    fetch("/dashboard/" + selection)
 
-         // convert html prototype into text
-         .then((response) => { return response.text(); })
+      // convert html prototype into text
+      .then((response) => { return response.text(); })
 
-         // insert html text into display div inner html and load script
-         .then((html) => {
-           displayElement.innerHTML = html;
-         })
+      // insert html text into display div inner html and load script
+      .then((html) => {
+        displayElement.innerHTML = html;
+      })
 
-         // catch and log error (if that somehow happens...)
-         .catch((error) => { console.error("Unable to load template: ", error); });
-    });
-  });
+      // catch and log error (if that somehow happens...)
+      .catch((error) => { console.error("Unable to load template: ", error); });
+ });
 });
