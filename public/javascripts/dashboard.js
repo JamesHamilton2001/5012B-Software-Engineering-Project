b755641a6a06491482e4006e30ee7843b6ba44c4
Joey Sabey <joey.sabey@gmx.com>
2023-05-31 05:12:16 +0100

    change .sideBarItem selector to menu > li in dasgboard.js

--- public/javascripts/dashboard.js
+++ public/javascripts/dashboard.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const displayElement = document.querySelector('body > main');
 
   // add event listener to each sidebar item, that changes the dashboard display accordingly
-  document.querySelectorAll(".sideBarItem").forEach((item) => {
+  document.querySelectorAll('menu > li').forEach((item) => {
     item.addEventListener("mousedown", function() {
 
       var selection;
