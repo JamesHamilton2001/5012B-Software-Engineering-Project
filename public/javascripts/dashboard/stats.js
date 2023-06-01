b9fe70f6fa3cf77084cb0d2d395716e5570856e1
JamesHamilton2001 <james.hamilton.2001@outlook.com>
2023-06-01 02:14:31 +0100

    dummy graph in dashboard stats

--- public/javascripts/dashboard/stats.js
+++ public/javascripts/dashboard/stats.js
@@ -1,46 +1,35 @@
-console.log("fml");
-export default class StatsChartHandler
+
+export class ChartManager
 {
-  constructor()
-  {
-    this.canvas;
+  constructor() {
+    this.chart;  
     this.chartData;
     this.chartOptions;
-    this.chart;
 
-    this.canvas = document.getElementById("chart_canvas");
+    const context = document.getElementById("chart_canvas").getContext("2d");
 
-    if (this.canvas.getContext) {
-      const context = this.canvas.getContext("2d");
-
-      this.chartData = {
-        labels: ["one", "two", "tre", "for"],
-        datasets: [{
-          label:        "data",
-          data:         [1,2,3,4],
-          borderColor:  "rgb(0, 255, 0)"
-        }, {
-          label:        "atad",
-          data:         [4,-3,2,-1],
-          borderColor:  "rgb(255, 0, 0)"
-        }]
-      };
-      this.chartOptions = {
-        scales: {
-          x: { grid: { display: false } },
-          y: { grid: { display: false } }
-        }
-      };
-      this.chart = new Chart(context, {
-        type: "line",
-        data: this.chartData,
-        options: this.chartOptions
-      });
-    }
-    else {
-      console.error("dashboard/stats.js unable to access chart canvas");
-    }
+    this.chartData = {
+      labels: ["one", "two", "tre", "for"],
+      datasets: [{
+        label:        "data",
+        data:         [1,2,3,4],
+        borderColor:  "rgb(0, 255, 0)"
+      }, {
+        label:        "atad",
+        data:         [4,-3,2,-1],
+        borderColor:  "rgb(255, 0, 0)"
+      }]
+    };
+    this.chartOptions = {
+      scales: {
+        x: { grid: { display: false } },
+        y: { grid: { display: false } }
+      }
+    };
+    this.chart = new Chart(context, {
+      type:    "line",
+      data:    this.chartData,
+      options: this.chartOptions
+    });
   }
 }
-
-var statsChartHandler = new StatsChartHandler();
