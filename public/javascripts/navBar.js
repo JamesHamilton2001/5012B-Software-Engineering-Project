51fbe9a6e8f4294a9933d7dc2250eac1e06fd079
j-laze <107155977+j-laze@users.noreply.github.com>
2023-06-02 02:45:34 +0100

    Moved information to navbar
    Moved greeting login message to navbar for a cleaner look

--- public/javascripts/navBar.js
+++ public/javascripts/navBar.js
@@ -1,3 +1,4 @@
+import * as api from "/js/api.js";
 // scales body top padding to match header height
 function scaleBodyPaddingTop() {
   document.body.style.paddingTop = `${document.querySelector("header").offsetHeight}px`;
@@ -8,3 +9,15 @@ document.addEventListener("DOMContentLoaded", function() {
   scaleBodyPaddingTop();
   window.addEventListener("resize", scaleBodyPaddingTop);
 });
+
+async function fetchData() {
+  try {
+    const Userdata = await api.get("user/");
+    //const Userexercise = await api.get("user/exercise");
+    //console.log(Userexercise);
+    console.log(Userdata.username);
+    return String (Userdata.username); // Process the Userdata or assign it to a variable
+  } catch (error) {
+    console.error(error); // Handle any errors that occur during the request
+  }}
+document.getElementById("User").textContent += await fetchData() + " !";
