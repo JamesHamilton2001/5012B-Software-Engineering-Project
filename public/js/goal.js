55950d330fc5b04d685fd1f3f3e0f25d1ea48431
Joey Sabey <joey.sabey@gmx.com>
2023-06-02 08:38:19 +0100

    format

--- public/js/goal.js
+++ public/js/goal.js
@@ -4,38 +4,38 @@ import * as exercise from './exercise.js';
 
 export const goals = await api.get('/goal');
 
-//create 2 forms, one for weightGoal, one for exerciseGoal
+
 export async function createWeightGoalForm() {
-  const form = document.createElement("form");
-  form.id = 'weightGoal';
+   const form = document.createElement("form");
+   form.id = 'weightGoal';
 
-  const fieldset = form.appendChild(document.createElement('fieldset'));
+   const fieldset = form.appendChild(document.createElement('fieldset'));
 
-  const legend = fieldset.appendChild(document.createElement('legend'));
-  legend.textContent = 'New Weight Goal';
+   const legend = fieldset.appendChild(document.createElement('legend'));
+   legend.textContent = 'New Weight Goal';
 
-  const target = fieldset.appendChild(ui.createNumericInput());
-  target.id = 'weightTarget';
+   const target = fieldset.appendChild(ui.createNumericInput());
+   target.id = 'weightTarget';
 
-  const endTime = fieldset.appendChild(document.createElement('input'))
-  endTime.id = 'weightEndTime';
-  endTime.type = 'date'
-  endTime.name = 'endTime'
+   const endTime = fieldset.appendChild(document.createElement('input'))
+   endTime.id = 'weightEndTime';
+   endTime.type = 'date'
+   endTime.name = 'endTime'
 
-  form.getData = () => {
-    return {
-      target: parseInt(target.value),
-      endTime: new Date(endTime.value),
-    };
- };
+   form.getData = () => {
+      return {
+         target: parseInt(target.value),
+         endTime: new Date(endTime.value),
+      };
+   };
 
-  const submit = fieldset.appendChild(document.createElement('input'))
-  submit.id = 'newWeightGoal'
-  submit.type = 'submit'
-  submit.value = 'Create Goal'
+   const submit = fieldset.appendChild(document.createElement('input'))
+   submit.id = 'newWeightGoal'
+   submit.type = 'submit'
+   submit.value = 'Create Goal'
 
-  submit.addEventListener('click', async () => {
-    // Disable changes to prevent e.g. multiple submissions
+   submit.addEventListener('click', async () => {
+      // Disable changes to prevent e.g. multiple submissions
       fieldset.disabled = true;
       // Send the data to the API
       const response = await api.post('goal', form.getData());
@@ -47,63 +47,63 @@ export async function createWeightGoalForm() {
       form.replaceWith(message);
    });
 
-  return form;
+   return form;
 }
 
 
 export async function createExerciseGoalForm() {
-  //create 2 forms, one for weightGoal, one for exerciseGoal
-  const form = document.createElement("form");
-  form.id = 'exerciseGoal';
+   //create 2 forms, one for weightGoal, one for exerciseGoal
+   const form = document.createElement("form");
+   form.id = 'exerciseGoal';
 
-  const fieldset = form.appendChild(document.createElement('fieldset'));
+   const fieldset = form.appendChild(document.createElement('fieldset'));
 
-  const legend = fieldset.appendChild(document.createElement('legend'));
-  legend.textContent = 'New Exercise Goal';
+   const legend = fieldset.appendChild(document.createElement('legend'));
+   legend.textContent = 'New Exercise Goal';
 
-  const target = fieldset.appendChild(exercise.createFieldset());
-  target.id = 'exerciseTarget';
+   const target = fieldset.appendChild(exercise.createFieldset());
+   target.id = 'exerciseTarget';
 
-  const endTime = fieldset.appendChild(document.createElement('input'))
-  endTime.id = 'exerciseEndTime';
-  endTime.type = 'date'
-  endTime.name = 'endTime'
+   const endTime = fieldset.appendChild(document.createElement('input'))
+   endTime.id = 'exerciseEndTime';
+   endTime.type = 'date'
+   endTime.name = 'endTime'
 
-  form.getData = () => {
-    return {
-      target: parseInt(target.getData()),
-      endTime: new Date(endTime.value),
-    };
- };
-  
-  const submit = fieldset.appendChild(document.createElement('input'))
-  submit.id = 'newExerciseGoal'
-  submit.type = 'submit'
-  submit.value = 'Create Goal'
+   form.getData = () => {
+      return {
+         target: parseInt(target.getData()),
+         endTime: new Date(endTime.value),
+      };
+   };
 
-  submit.addEventListener('click', async () => {
-    // Disable changes to prevent e.g. multiple submissions
-    fieldset.disabled = true;
-    // Send the data to the API
-    const response = await api.post('goal', form.getData());
-    // TODO: handle response better than just logging
-    console.log(response);
-    // TODO: go fix the api.post() function to return a better object (e.g. include status, etc?)
-    const message = document.createElement('p');
-    message.textContent = 'Goal added successfully!';
-    form.replaceWith(message);
-  });
+   const submit = fieldset.appendChild(document.createElement('button'))
+   submit.id = 'newExerciseGoal'
+   submit.type = 'button'
 
-  return form;
+   submit.addEventListener('click', async () => {
+      // Disable changes to prevent e.g. multiple submissions
+      fieldset.disabled = true;
+      // Send the data to the API
+      const response = await api.post('goal', form.getData());
+      // TODO: handle response better than just logging
+      console.log(response);
+      // TODO: go fix the api.post() function to return a better object (e.g. include status, etc?)
+      const message = document.createElement('p');
+      message.textContent = 'Goal added successfully!';
+      form.replaceWith(message);
+   });
+
+   return form;
 }
 
 
 export async function displayGoals() {
-  const div = document.createElement("div");
+   const div = document.createElement("div");
 
-  goals.forEach((goal) => {
-    console.log(goals.target)
-  });
+   goals.forEach((goal) => {
+      console.log(goals.target)
+   });
 
-  return div;
+   return div;
 }
+
